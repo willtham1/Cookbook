dba248f481134f4e6bc2134ee14839286c8e5a8c
Alex Camacho <alsbrain@optonline.net>
2020-11-07 13:53:46 -0800

    added previous and next search buttons

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
+var pageNumber
+var recipeList
 
-// Recipe puppy API starter
-// variable will be used to store user input later on.
-
-$('#search-button').on('click', function(event){
+$('#previous-button').on('click', function(event){
     event.preventDefault()
-    var userInput = $('#userInput').val().trim()
-    userInput = userInput.replace(/ +/g, "")
-var recipeURL = `https://cors-anywhere.herokuapp.com/recipepuppy.com/api/?pg=1&i=${userInput}`;
-console.log(userInput)
-      $.ajax({
-        url: recipeURL,
-        method: "GET",
-      }).then(function (response) {
-        console.log(JSON.parse(response));
-
-        // Grab response with JSON, due to the response being returned as a string
-        var recipeList = JSON.parse(response)
-        for (var i = 0; i < recipeList.results.length; i++){
-            console.log('-------------------------')
-            console.log('Recipe name: ' + recipeList.results[i].title.trim())
-            console.log('Ingredient list: ' + recipeList.results[i].ingredients)
-            console.log('Thumbnail: ' + recipeList.results[i].thumbnail)
-            console.log('Link: ' + recipeList.results[i].href)
-            console.log('-------------------------')
-        
-        }
-      });
+    console.log('test')
+    if(pageNumber >1){
+        pageNumber--
+        generateRecipes()
+    } else {
+        console.log('error')
+    } 
 })
 
+$('#next-button').on('click', function(event){
+    event.preventDefault()
+    console.log('Next')
+    pageNumber++
+    generateRecipes()
+
+})
+
+
+// Recipe puppy API
+$('#search-button').on('click', function(event){
+    event.preventDefault()
+    // Start on page one of results
+    pageNumber = 1
+    // Remove erroneous text from user string    
+    console.log(userInput)    
+    generateRecipes()
+    
+})
+
+// Add functionality to display the next page of results
 
 
 
@@ -38,15 +43,41 @@ console.log(userInput)
 var ingredientURL = `https://api.spoonacular.com/food/ingredients/substitutes?apiKey=fdea7c2ea8d1434eb3207d8b48260907&ingredientName=flour`
 
 $.ajax({
-
+    
     url: ingredientURL,
     method: 'GET'
 }).then(function (response) {
-console.log(response.message)
+    console.log(response.message)
     for (var i = 0; i < response.substitutes.length; i++){
         console.log(response.substitutes[i])
-
+        
     }
-
-
+    
+    
 })
+
+function generateRecipes() {
+    var userInput = $('#userInput').val().trim()
+    userInput = userInput.replace(/ +/g, "")
+    var recipeURL = `https://cors-anywhere.herokuapp.com/recipepuppy.com/api/?p=${pageNumber}&i=${userInput}`;
+    console.log(recipeURL)
+    $.ajax({
+        url: recipeURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(JSON.parse(response));
+
+        // Grab response with JSON, due to the response being returned as a string
+        recipeList = JSON.parse(response)
+        for (var i = 0; i < recipeList.results.length; i++) {
+            console.log('-------------------------')
+            console.log('Recipe name: ' + recipeList.results[i].title.trim())
+            console.log('Ingredient list: ' + recipeList.results[i].ingredients)
+            console.log('Thumbnail: ' + recipeList.results[i].thumbnail)
+            console.log('Link: ' + recipeList.results[i].href)
+            console.log('-------------------------')
+        };
+    })
+}
+
+    
